4e5afb2de1de49f69c8ca8b6ea97ff331d0ef4ab
Amirijaz23 <amirkhantech1@gmail.com>
2026-05-15 17:01:18 +0500

    Modal Update

--- src/components/booknow/Modal.jsx
+++ src/components/booknow/Modal.jsx
@@ -28,7 +28,7 @@ const Modal = () => {
         profile.showModal &&
         <div className='w-full px-4 md:px-0 h-full flex justify-center absolute top-20 backdrop-blur-[5px]'>
 
-            <div className='bg-white h-140 md:h-100 w-140  mx-auto rounded-2xl overflow-hidden sticky top-40'>
+            <div className='bg-white h-140 md:h-100 w-140  mx-auto rounded-2xl overflow-hidden fixed top-40'>
                 <div className='flex items-center gap-4 bg-linear-to-r from-[#2D2517] to-[#2d251794] p-6 pe-10 relative'>
                     <div className='bg-[#D4AF37] w-12 h-12 flex justify-center items-center rounded-full text-white shrink-0'>
                         <CiUser className='text-[22px]' />
